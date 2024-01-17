d01d0401366157b5ecbabf289a4f0c9f04ec5557
DouglasPazDaSilva <programador03sundownpark@gmail.com>
2024-01-17 10:02:02 -0300

    feat:adiciona

--- src/myModule.ts
+++ src/myModule.ts
@@ -5,3 +5,15 @@ export function add(a: number, b: number) {
 export function subtract(a: number, b: number) {
   return a - b;
 }
+
+export function formatCPF(cpf: string) {
+  if (cpf.length >= 14) {
+    cpf = cpf.slice(0, 14)
+    return cpf
+  }
+  cpf = cpf.replace(/\D/g, '') // Remove tudo o que não é dígito
+  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2') // Coloca um ponto entre o terceiro e o quarto dígitos
+  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2') // Coloca um ponto entre o terceiro e o quarto dígitos
+  cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2') // Coloca um hífen entre o terceiro e o quarto dígitos
+  return cpf
+}
